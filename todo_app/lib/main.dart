2632958dbccb308717a7fbe8f8ed3e58d0ad6ad4
Inoh Pak <pio2725@gmail.com>
2021-09-10 00:03:26 -0600

    small changes

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(brightness: Brightness.light),
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
